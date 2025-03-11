a6557cf3f27bfa51e655ab543faea1bc457cece7
mediwateyash <diwateyash2004@gmail.com>
2025-03-11 22:59:21 +0530

    corosel added

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
     });
   });
+
+  // Carousel Functionality
+let currentSlide = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll('.carousel-item');
+  if (index >= slides.length) currentSlide = 0;
+  if (index < 0) currentSlide = slides.length - 1;
+
+  const carouselInner = document.querySelector('.carousel-inner');
+  carouselInner.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+  currentSlide++;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide--;
+  showSlide(currentSlide);
+}
+
+// Auto-play the carousel (optional)
+setInterval(nextSlide, 5000); // Change slide every 5 seconds
